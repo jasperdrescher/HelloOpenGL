c7853d7cca8b3c8105c07bb55b80f83c3b62d649
Jasper Drescher <jasperdre@gmail.com>
2023-03-14 23:19:56 +0100

    Rename CMake folder

--- CMake/glad.cmake
+++ CMake/glad.cmake
@@ -1,5 +1,5 @@
 set(GLAD_DIR "${SUBM_DIR}/GLAD")
 add_subdirectory("${GLAD_DIR}/cmake")
-glad_add_library(glad_gl_core_46 STATIC REPRODUCIBLE API gl:core=4.6)
-target_link_libraries(${PROJECT_NAME} glad_gl_core_46)
+glad_add_library(glad STATIC REPRODUCIBLE API gl:core=4.6)
+target_link_libraries(${PROJECT_NAME} glad)
 target_include_directories(${PROJECT_NAME} PRIVATE "${GLM_DIR}")
